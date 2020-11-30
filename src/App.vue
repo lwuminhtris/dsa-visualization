e4a6c64b2b01d672987b2abe0ac06e03cde6f16c
Your Name <you@example.com>
2020-11-30 19:48:36 +0700

    update ui

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template >
+  <v-app style="background-color: black;">
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
