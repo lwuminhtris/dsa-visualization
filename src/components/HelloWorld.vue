5466737772498c948dabe5382b6ce6763cfe80f0
notm16 <minhtri.luu10@gmail.com>
2020-12-02 10:48:31 +0700

    heap construction

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -30,13 +30,63 @@
               style = "margin-left: 20px; margin-right: 20px; margin-top: 20px;" 
               dark
             >
-              
             </v-text-field>
+            <v-btn
+              dark
+              style = "margin-left: 20px; margin-right: 20px; margin-top: -30px;" 
+              width="93.5%"
+            >
+            <v-icon>
+              mdi-import
+            </v-icon>
+              CREATE BINARY SEARCH TREE
+            </v-btn>
           </v-tab-item>
 
           <v-tab-item>
-
+            <v-text-field
+              placeholder="Your array"
+              solo
+              style = "margin-left: 20px; margin-right: 20px; margin-top: 20px;" 
+              dark
+            >
+            </v-text-field>
+            <v-btn
+              dark
+              style = "margin-left: 20px; margin-right: 20px; margin-top: -30px;" 
+              width="93.5%"
+            >
+            <v-icon>
+              mdi-import
+            </v-icon>
+              CREATE AVL TREE
+            </v-btn>
           </v-tab-item>
+
+          <v-tab-item
+            
+          >
+            <v-text-field
+              placeholder="Your array"
+              solo
+              style = "margin-left: 20px; margin-right: 20px; margin-top: 20px;" 
+              dark
+            >
+            </v-text-field>
+            <v-btn
+              color="primary"
+              style = "margin-left: 20px; margin-right: 20px; margin-top: -30px;" 
+              width="93.5%"
+              @click="createHeap"
+            >
+            <v-icon>
+              mdi-import
+            </v-icon
+            >
+              CREATE HEAP
+            </v-btn>
+          </v-tab-item>
+
         </v-tabs>
         </v-card>
       </v-col>
@@ -52,7 +102,14 @@
 
     data: () => ({
       items: ['Binary Search Tree', 'AVL Tree', 'Heap', 'Hash'],
-      detail: ['BST', 'AVL', 'HEAP', 'HASH']
+      detail: ['BST', 'AVL', 'HEAP', 'HASH'],
+      arr: String,
     }),
+    
+    methods: {
+      createHeap() {
+        alert("JUST CLICK HERE")
+      }
+    },
   }
 </script>
